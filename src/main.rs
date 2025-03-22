3f287b6cd9501a2df3f2658f99b6e80475fe8ac7
PedroFranca2299 <pedrofranca-tech@proton.me>
2025-03-21 21:51:14 -0300

    feat: add word counting function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::env;
 use std::fs;
-// use std::io::BufReader;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,11 +15,28 @@ fn main() {
 fn handle_operation(operation: &String, file_content: String) {
     match operation.as_str() {
         "-c" => {
-            println!(
-                "This file is {} characters long",
-                file_content.chars().count()
-            )
+            let word_count = word_counter(file_content);
+            println!("This file is {} words long", &word_count)
         }
         _ => println!("Unknown operation"),
     }
+}
+
+fn word_counter(file_content: String) -> u128 {
+    let characters: Vec<char> = file_content.chars().collect();
+    let mut word_count: u128 = 0;
+    for i in 0..characters.len() - 1 {
+        let current = characters[i];
+        let next = characters[i + 1];
+        word_count += if current.is_whitespace() {
+            0
+        } else {
+            if !current.is_whitespace() && next.is_whitespace() {
+                1
+            } else {
+                0
+            }
+        }
+    }
+    word_count
 }
